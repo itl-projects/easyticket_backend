51abed7cdc24a72122b1cae7fe885935da880f8e
Bhola Kumar <sfs.bhola21@gmail.com>
2021-08-03 21:42:05 +0530

    setting changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,16 +13,18 @@ const entities = [User, UserProfile];
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as any,
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: entities,
-      synchronize: true,
-      autoLoadEntities: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: process.env.DB_TYPE as any,
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: entities,
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
     }),
     UsersModule,
     AuthModule,
